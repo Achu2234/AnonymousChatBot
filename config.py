0a01c17f5be63adfb4441d7c648e3cca4d061e59
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-07-29 12:27:38 +0530

    Added Channel and group

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+
+TOKEN = os.environ.get("TOKEN", "")
+OWNER = os.environ.get("OWNER", "AmiFutami")
+GROUP = os.environ.get("GROUP", "safothebot")
+CHANNEL = os.environ.get("CHANNEL", "AsmSafone")
+BOT_NAME = os.environ.get("BOT_NAME", "Anonymous ChatBot")
